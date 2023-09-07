5d3d66ca905ed43840e89362be075558b04de1ad
j10c <blyb1739@gmail.com>
2023-09-07 21:31:24 +0800

    feat(ui): update counter example style

--- packages/ui/src/components/Counter.tsx
+++ packages/ui/src/components/Counter.tsx
@@ -2,16 +2,26 @@ import { useState } from "react";
 import { View, Text, Button } from "@tarojs/components";
 
 const Counter = () => {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(1000);
 
   const handleClick = () => {
-    setCount(count + 1);
+    setCount(count + 10);
   }
 
   return (
-    <View>
-      <Text style={{ fontWeight: "500" }}> { count } </Text>
-      <Button onClick={handleClick}>count++</Button>
+    <View className="counter animate-fade-in animate-duration-100">
+      <View className="m-4 bg-white shadow rd-4 px-6 py-4">
+        <View className="flex items-center gap-2 mb-2">
+          <View className="bg-blue rd-4 w-2 h-2" />
+          <View className="font-600 text-[36rpx]">COUNT</View>
+        </View>
+        <View className="flex items-center gap-2 c-gray-500">
+          <Text className="i-mdi-calculator text-[36rpx]" />
+          <Text className="text-5" > { count } </Text>
+        </View>
+        <View className="h-14" />
+        <Button onClick={handleClick}>count + 10</Button>
+      </View>
     </View>
   )
 }
